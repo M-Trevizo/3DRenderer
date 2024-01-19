61974d7f9204e797a96d03e6b9efe9d1a4e1ff56
Michael Trevizo <michael.trevizo93@gmail.com>
2024-01-18 21:03:38 -0800

    Add class for handling matrix transformation and drawing

--- DemoViewer.java
+++ DemoViewer.java
@@ -58,13 +58,34 @@ public class DemoViewer {
 
                 // Translate origin from upper left corner to the center before drawing
                 g2d.translate(getWidth() / 2, getHeight() / 2);
-                
+
                 g2d.setColor(Color.WHITE);
                 for(Triangle t : triangles) {
                     Path2D path = new Path2D.Double();
                     path.moveTo(t.v1.x, t.v1.y);
                     path.lineTo(t.v2.x, t.v2.y);
                     path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2d.draw(path);
+                }
+
+                double heading = Math.toRadians(headingSlider.getValue());
+                Matrix3 transform = new Matrix3(new double[] {
+                    Math.cos(heading), 0, -Math.sin(heading),
+                    0, 1, 0,
+                    Math.sin(heading), 0, Math.cos(heading)
+                });
+
+                g2d.translate(getWidth() / 2, getHeight() / 2);
+                g2d.setColor(Color.WHITE);
+                for(Triangle t : triangles) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
                     g2d.draw(path);
                 }
@@ -102,5 +123,33 @@ class Triangle {
         this.v2 = v2;
         this.v3 = v3;
         this.color = color;
+    }
+}
+
+class Matrix3 {
+
+    double[] values;
+
+    Matrix3(double[] values) {
+        this.values = values;
+    }
+
+    Matrix3 multiply(Matrix3 other) {
+        double[] result = new double[9];
+        for(int row = 0; row < 3; row++) {
+            for(int col = 0; col < 3; col++) {
+                for(int i = 0; i < 3; i++) {
+                    result[row * 3 + col] += this.values[row * 3 + i] * other.values[i * 3 + col];
+                }
+            }
+        }
+        return new Matrix3(result);
+    }
+
+    Vertex transform(Vertex in) {
+        return new Vertex(
+            in.x * values[0] + in.y * values[3] + in.z * values[6],
+            in.x * values[1] + in.y * values[4] + in.z * values[7],
+            in.x * values[2] + in.y * values[5] + in.z * values[8]);
     }
 }
